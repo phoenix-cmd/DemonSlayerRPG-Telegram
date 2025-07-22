68914663905068d130e4c19e594f51ba5e753596
phoenix-cmd <73835333+phoenix-cmd@users.noreply.github.com>
2025-07-22 17:16:04 +0530

    Update player.py

--- models/player.py
+++ models/player.py
@@ -1,1 +1,13 @@
+# models/player.py
 
+player_schema = {
+    "telegram_id": int,
+    "name": str,
+    "gender": str,     # 'male' or 'female'
+    "mode": str,       # 'story' or 'explore'
+    "level": int,
+    "exp": int,
+    "location": str,
+    "items": list,
+    "created_at": "datetime"
+}
