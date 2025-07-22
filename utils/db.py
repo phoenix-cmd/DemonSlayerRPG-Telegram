ec0ffb46ffa265e3dd8cf101c6dc24eca976a6d7
phoenix-cmd <73835333+phoenix-cmd@users.noreply.github.com>
2025-07-22 17:16:35 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,10 @@
+from pymongo import MongoClient
+
+def get_db():
+    client = MongoClient("mongodb://localhost:27017/")
+    return client['demon_slayer_rpg']
+
+def get_players():
+    db = get_db()
+    return db.players
 
